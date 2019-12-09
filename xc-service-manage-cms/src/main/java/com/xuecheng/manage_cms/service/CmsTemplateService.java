5f3acb0d16bf01a690a8bbb8df019f0ff731e509
bean95 <761632370@qq.com>
2019-12-10 01:43:25 +0800

    新增CmsSite、CmsTemplate - findAll()接口及实现

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/CmsTemplateService.java
@@ -0,0 +1,29 @@
+package com.xuecheng.manage_cms.service;
+
+import com.xuecheng.framework.domain.cms.CmsTemplate;
+import com.xuecheng.framework.model.response.CommonCode;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.framework.model.response.QueryResult;
+import com.xuecheng.manage_cms.dao.CmsTemplateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CmsTemplateService {
+
+    @Autowired
+    private CmsTemplateRepository cmsTemplateRepository;
+
+    public QueryResponseResult findAll(){
+
+        List<CmsTemplate> all = cmsTemplateRepository.findAll();
+        QueryResult<CmsTemplate> queryResult = new QueryResult<>();
+        queryResult.setList(all);
+        queryResult.setTotal(all.size());
+        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS,queryResult);
+        return queryResponseResult;
+
+    }
+}
